7f0313177bc04a6bb34eab9f894d59bf0e2cc71a
alojine <benaspaliokas@gmail.com>
2024-05-06 22:03:05 +0300

    init

---
+++ src/main/kotlin/com/re/traffer/TrafferApplication.kt
@@ -0,0 +1,11 @@
+package com.re.traffer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrafferApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrafferApplication>(*args)
+}
---
+++ src/test/kotlin/com/re/traffer/TrafferApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.re.traffer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrafferApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
